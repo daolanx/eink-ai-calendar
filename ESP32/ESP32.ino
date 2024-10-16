b13177bc623a235a5337f75cf54e75329feab091
daolanx <daolanx@hotmail.com>
2024-10-16 23:05:17 +0800

    feat(updateInk): When the power is turned on, it will also perform an update.

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -8,93 +8,41 @@
 #include "EInk.h"
 #include "time.h"
 
-#define MAX_WIFI_CONNECT_RETRY 20
-#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 60 * 60 * 1     /* Time ESP32 will go to sleep (in seconds) - 1 hour */
+#define MAX_WIFI_CONNECT_RETRY 20  /* WiFi连接最大重试次数 */
+#define US_TO_S_FACTOR 1000000     /* 微秒到秒的转换因子 */
+#define TIME_TO_SLEEP (60 * 60 * 1) /* ESP32将睡眠的时间（秒） - 1小时 */
 
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 8 * 3600;
-const int   daylightOffset_sec = 0;
-
-void printLocalTime() {
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        Serial.println("Failed to obtain time");
-        return;
-    }
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-bool isEarlyMorning() {
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        Serial.println("Failed to obtain time");
-        return false;
-    }
-    Serial.print("Current hour: ");
-    Serial.println(timeinfo.tm_hour);
-    // 检查是否在凌晨时段
-    return (timeinfo.tm_hour == 0);
-}
+#define NTP_SERVER "pool.ntp.org"   /* NTP服务器地址 */
+#define GMT_OFFSET_SEC (8 * 3600)   /* GMT偏移量（秒） - 东八区 */
+#define DAYLIGHT_OFFSET_SEC 0       /* 夏令时偏移量（秒） */
 
 void setup() {
     Serial.begin(115200);
     delay(1000);  // 给一些时间让串口准备好
     Serial.println("ESP32 starting up...");
 
-    esp_sleep_wakeup_cause_t wakeup_reason;
-    wakeup_reason = esp_sleep_get_wakeup_cause();
-    switch(wakeup_reason)
-    {
-        case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-        case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-        case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-        case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-        case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-        default : Serial.println("Wakeup was not caused by deep sleep"); break;
-    }
-
-    // connect to wifi
-    int wifi_connect_try = 0;
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting to WiFi...");
-    while (WiFi.status() != WL_CONNECTED && wifi_connect_try < MAX_WIFI_CONNECT_RETRY) {
-        delay(500);
-        Serial.print(".");
-        wifi_connect_try++;
-    }
-    Serial.println();
-
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.print("Connected, IP address: ");
-        Serial.println(WiFi.localIP());
-
-        // 初始化并同步网络时间
-        configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-        Serial.println("Waiting for time sync...");
-        delay(2000);  // 给一些时间让时间同步
-
-        // 打印本地时间
-        printLocalTime();
-
-        // 检查是否在凌晨时段
-        if (isEarlyMorning()) {
-            Serial.println("It's early morning. Updating E-Ink display.");
-            updateEInk();
-        } else {
-            Serial.println("Not early morning. Skipping E-Ink update.");
-        }
-
-       
-    } else {
+    if (!connectWiFi()) {
         Serial.println("Failed to connect to WiFi");
+        return;
     }
 
-     // 断开 WiFi 连接
-        WiFi.disconnect(true);
+    // 初始化并同步网络时间
+    configTime(DAYLIGHT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+    Serial.println("Waiting for time sync...");
+    delay(3000);  // 给一些时间让时间同步
+
+    // 检查是否在凌晨时段, 或者是否是重启
+    bool isEarly = isEarlyMorning();
+    bool isPowerOn = isRestByPowerOn();
+    if (isEarly || isPowerOn) {
+        Serial.println("Updating E-Ink display. Reason: " + String(isEarly ? "Early morning" : "Restarted by power on"));
+        updateEInk();
+    } else {
+        Serial.println("Skip E-Ink update. Reason: Not early morning and not restarted by power on");
+    }
 
     // 设置定时唤醒
-    esp_sleep_enable_timer_wakeup((uint64_t)TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    esp_sleep_enable_timer_wakeup((uint64_t)TIME_TO_SLEEP * US_TO_S_FACTOR);
     Serial.println("Setup ESP32 to sleep for " + String(TIME_TO_SLEEP) + " seconds");
     
     // 进入深度睡眠
@@ -103,10 +51,51 @@ void setup() {
 }
 
 void loop() {
+    // 深度睡眠模式下，loop 函数不会被执行
+}
+
+bool connectWiFi() {
+    int wifi_connect_try = 0;
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED && wifi_connect_try < MAX_WIFI_CONNECT_RETRY) {
+        delay(500);
+        Serial.print(".");
+        wifi_connect_try++;
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.print("Connected, IP address: ");
+        Serial.println(WiFi.localIP());
+        return true;
+    } else {
+        Serial.println("Failed to connect to WiFi");
+        return false;
+    }
+}
+
+bool isEarlyMorning() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return false;
+    }
+    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+    Serial.println("Current hour: " + String(timeinfo.tm_hour));
+    // 检查是否在凌晨时段
+    bool isEarlyMorning = timeinfo.tm_hour == 0;
+    Serial.println("isEarlyMorning: " + String(isEarlyMorning));
+    return isEarlyMorning;
+}
+
+bool isRestByPowerOn() {
+    esp_reset_reason_t reset_reason = esp_reset_reason();
+    Serial.println("Reset reason: " + String(reset_reason));
+    return (reset_reason == ESP_RST_POWERON);
 }
 
 void updateEInk() {
-    String req = (String)host + "/bytes";
+    String req = String(host) + "/bytes";
     HTTPClient http;
     http.begin(req);
     
@@ -129,18 +118,15 @@ void updateEInk() {
         int offset = 0;
         WiFiClient *stream = http.getStreamPtr();
         stream->setTimeout(60000);
-        while (http.connected() && (offset < IMAGE_SIZE)) 
-        {
-          size_t size = stream->available();
-          if (size)
-          {
+        while (http.connected() && (offset < IMAGE_SIZE)) {
+            size_t size = stream->available();
+            if (size) {
                 int c = stream->readBytes(temp_buff, ((size > sizeof(temp_buff)) ? sizeof(temp_buff) : size));
-                for (int i = 0; i < c; i++)
-                {
+                for (int i = 0; i < c; i++) {
                     EPD_5IN65F_SendData(temp_buff[i]); // Send received data
                 }
                 offset += c;
-                Serial.println("received: " + (String)offset + "/" + (String)IMAGE_SIZE);
+                Serial.println("received: " + String(offset) + "/" + String(IMAGE_SIZE));
                 delay(10);
             }
         }
@@ -148,7 +134,6 @@ void updateEInk() {
         EPD_5IN65F_Display_end();
         EPD_5IN65F_Sleep();
         Serial.println("update E-Ink done");
-
     } else {
         Serial.println("update E-Ink failed. HTTP code: " + String(httpCode) + ", Length: " + String(len));
     }
